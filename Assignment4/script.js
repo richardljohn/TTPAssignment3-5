59d1124c777d1e372cef06466b1908df47dd2032
richardljohn <richardljohn@gmail.com>
2021-01-09 11:56:24 -0500

    Question 2-3 Assignment 4

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -1,3 +1,4 @@
+
 const B1 = document.getElementById("Button1");
 const B2 = document.getElementById("Button2");
 
@@ -9,4 +10,27 @@ Button1.onclick = function() {
 Button2.onclick = function(){
     message.innerText = "No, I'm right!";
     console.log(message);
+}
+
+//Question #2
+
+const doNot = document.getElementById("Don't Hover");
+
+function warn(){
+    alert("I TOLD YOU NOT TO HOVER OVER ME!");
+}
+
+//Question #3
+
+const B3 = document.getElementById("account");
+const pass = document.getElementById("password")
+
+B3.onclick = function() {
+    if(pass.innerText === "12345678"){
+        valid.innerText = "Welcome!";
+        console.log(valid);
+    }
+    else {
+        alert("Incorrect Password!");
+    }
 }
